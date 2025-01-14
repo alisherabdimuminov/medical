d1914cfb736ded366c57adada76e99b44de17755
alishercpp <alisher.abdimuminov.2005@gmail.com>
2025-01-14 15:23:30 +0500

    bug fix

--- config/settings.py
+++ config/settings.py
@@ -18,12 +18,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'corsheaders',
+
     'app',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -74,6 +77,9 @@ USE_TZ = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Additional settings
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CSRF_TRUSTED_ORIGINS = ["https://samendomarkaz.uz", "http://localhost:8000"]
 
 STATIC_URL = 'static/'
 STATIC_ROOT = "staticfiles"
